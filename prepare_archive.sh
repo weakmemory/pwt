683f586e21e35e40ebb979c5ac72360d4dabc8f4
Anton Podkopaev <podkoav239@gmail.com>
2021-10-04 17:18:31 +0300

    VM: added link to CoqIDE to the desktop

--- prepare_archive.sh
+++ prepare_archive.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 proofs=$(find src -name '*.v')
-vagrant_files="vagrant/Vagrantfile vagrant/pg_config/emacs_config vagrant/pg_config/install_packages.el vagrant/pg_config/setup_emacs.sh"
+vagrant_files="vagrant/Vagrantfile vagrant/pg_config/emacs_config vagrant/pg_config/install_packages.el vagrant/pg_config/setup_emacs.sh vagrant/coqide.desktop"
 top_files="configure Makefile README.md LICENSE.md"
 
 rm artifact.zip || true

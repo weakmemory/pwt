2335f5e3badbefc3a870f4454a04197ac05c2a38
Simon Cooksey <sjc205@kent.ac.uk>
2021-10-06 17:20:50 +0100

    add pwter to Vagrant

--- prepare_archive.sh
+++ prepare_archive.sh
@@ -3,6 +3,7 @@
 proofs=$(find src -name '*.v')
 vagrant_files="vagrant/Vagrantfile vagrant/pg_config/emacs_config vagrant/pg_config/install_packages.el vagrant/pg_config/setup_emacs.sh vagrant/coqide.desktop"
 top_files="configure Makefile README.md LICENSE.md"
+pwter="pomsets-with-predicate-transformers/"
 
 rm artifact.zip || true
-zip artifact $proofs $vagrant_files $top_files
+zip artifact -r $proofs $vagrant_files $top_files $pwter
